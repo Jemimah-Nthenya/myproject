b2ba14548c6a3d1f47bc1ba10835a4d8c50bebe3
Jemimah Nthenya <jemimahogonda@gmail.com>
2024-03-25 23:14:13 +0300

    add changes

---
+++ script.js
@@ -0,0 +1,5 @@
+var clickMeButton = document.createElement('button');
+clickButton.id = 'myButton';
+clickButton.innerHTML = 'Click Me';
+clickButton.style.background = '#4FFF8F';
+document.body.appendChild(clickMeButton);
