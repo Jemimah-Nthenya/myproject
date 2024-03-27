138e1a8f9c6829b3c5ed07659623e92921def5ce
Jemimah Nthenya <jemimahogonda@gmail.com>
2024-03-27 23:05:48 +0300

    add changes

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
-var clickMeButton = document.createElement('button');
-clickButton.id = 'myButton';
-clickButton.innerHTML = 'Click Me';
-clickButton.style.background = '#4FFF8F';
-document.body.appendChild(clickMeButton);
+function move_img(str) {
+    var step=50; // change this to different step value
+    switch(str){
+    case "down":
+    var x=document.getElementById('i1').offsetTop;
+    x= x + step;
+    document.getElementById('i1').style.top= x + "px";
+    break;
