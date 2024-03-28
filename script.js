3333434feed4f50e78c2b41a012148425bab81d6
Jemimah Nthenya <jemimahogonda@gmail.com>
2024-03-28 23:50:15 +0300

    add changes

--- script.js
+++ script.js
@@ -1,8 +1,5 @@
-function move_img(str) {
-    var step=50; // change this to different step value
-    switch(str){
-    case "down":
-    var x=document.getElementById('i1').offsetTop;
-    x= x + step;
-    document.getElementById('i1').style.top= x + "px";
-    break;
+next.addEventlistener("click",()=>
+scrollcontainer.addEventlistener=
+scrollcontainer,scrollleft+ =100
+)
+ProgressEvent.addEventlistener("click"())
